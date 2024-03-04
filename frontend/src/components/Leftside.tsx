ef17e55fbaa5e19542a3bd0cbec7ddb7079b8e3f
Sourabh Kumar <88062242+Sourabh25002@users.noreply.github.com>
2024-03-04 19:25:24 +0530

    Update Leftside.tsx

--- frontend/src/components/Leftside.tsx
+++ frontend/src/components/Leftside.tsx
@@ -12,9 +12,10 @@ const Leftside: React.FC = () => {
     const fetchProfileDetails = async () => {
       try {
         // Make GET request to fetch profile details
-        const response = await axios.get(backendUrl + "/user/profile/details", {
-          withCredentials: true, // Include cookies in the request
-        });
+        const response = await axios.get(backendUrl + "/user/profile/details", credentials, {
+        withCredentials: true,
+        credentials: 'include',
+      });
 
         // Set profile details in state
         setProfileDetails(response.data);
